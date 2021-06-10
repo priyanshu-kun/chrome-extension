33b7b9d489086b4f4b6a2134cf392be570271f48
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2021-06-10 21:41:02 +0530

    initial commit

---
+++ content.js
@@ -0,0 +1,11 @@
+// const blocked_application = document.body;
+
+// switch (window.location.hostname) {
+//     case "www.youtube.com":
+//         blocked_application.innerHTML = `<div style="padding: 20px; background: #000; border-radius: 10px; margin: 50px auto 0 auto; display: flex; justify-content: center; align-items: center; width: 60%;">
+//             <h1 style="text-align: center; color: rgba(255,255,255,0.6); font-size: 6rem; ">404 Page not found</h1>
+//         </div>`
+//     default:
+//         break;
+// }
+
